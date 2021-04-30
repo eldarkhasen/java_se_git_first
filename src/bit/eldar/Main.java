f052d1c80ed0b8bc488f328c3563d1e83154a017
eldarkhasen <eldarkhasen@gmail.com>
2021-04-30 18:02:29 +0600

    Second commit

--- src/bit/eldar/Main.java
+++ src/bit/eldar/Main.java
@@ -1,8 +1,13 @@
 package bit.eldar;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int i = in.nextInt();
+        System.out.println(i*i);
 
     }
 }
